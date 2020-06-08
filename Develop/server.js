58a5cca0bfa716fd6f3e9ddca07c75b4d8b5f7d2
Eze <eze.largaespada@gmail.com>
2020-06-08 19:48:07 -0400

    server

--- Develop/server.js
+++ Develop/server.js
@@ -7,9 +7,15 @@ const PORT = process.env.PORT || 8080;
 
 app.use(morgan("dev"));
 
+app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static('public'));
+
+
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI,{  
@@ -17,8 +23,7 @@ mongoose.connect(MONGODB_URI,{
     useFindAndModify:false
 })
 
-require("./routes/api-routes")(app);
-require("./routes/html-routes")(app);
+
 
 app.listen(PORT,function(){ 
     console.log(`App listening on Port ${PORT}`);
